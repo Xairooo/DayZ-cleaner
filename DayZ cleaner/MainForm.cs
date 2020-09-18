e95145ff422a255a293cd1717512409f0708da74
Xairo <xairooo@gmail.com>
2020-09-18 22:28:11 +0200

    Dotnet Core 3.0

--- DayZ cleaner/MainForm.cs
+++ DayZ cleaner/MainForm.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DayZ_cleaner
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        private static List<string> fileExts = new List<string>(){ "mdmp", "rpt", "log" };
-        List<string> files = new List<string>();
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
+
+        private static readonly List<string> fileExts = new List<string> {"mdmp", "rpt", "log"};
+        private readonly List<string> files = new List<string>();
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -28,7 +25,7 @@ namespace DayZ_cleaner
         private void okButton_Click(object sender, EventArgs e) => deleteFiles();
         private void cancelButton_Click(object sender, EventArgs e) => Application.Exit();
 
-        async Task deleteFiles()
+        private async Task deleteFiles()
         {
             foreach (var file in files)
             {
@@ -41,55 +38,56 @@ namespace DayZ_cleaner
                     Console.WriteLine(e);
                 }
             }
+
             Application.Exit();
         }
-        async Task findFiles()
+
+        private async Task findFiles()
         {
             var path = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\DayZ\\";
-            
+
             long fileSize = 0;
 
             foreach (var fileExt in fileExts) files.AddRange(Directory.GetFiles(path, $"*.{fileExt}"));
             foreach (var file in files) fileSize += new FileInfo(file).Length;
-            
+
             await Task.Delay(1);
             ResultTextBox.Text = $"Found {files.Count} files with a size of {BytesToPrettyString(fileSize)}.";
         }
 
-        public static string BytesToPrettyString(object Size) => ConvertBytesToPrettyString((float)Convert.ToDouble(Size), 0);
-        public static string BytesToPrettyString(double Size) => ConvertBytesToPrettyString((float)Size, 0);
         public static string BytesToPrettyString(float Size) => ConvertBytesToPrettyString(Size, 0);
-        public static string BytesToPrettyString(int Size) => ConvertBytesToPrettyString(Size, 0);
 
         private static string ConvertBytesToPrettyString(float Size, int R)
         {
-            float F = Size / 1024f;
+            var F = Size / 1024f;
             if (F < 1)
             {
                 switch (R)
                 {
                     case 0:
-                        return string.Format("{0:0.00} byte", Size);
+                        return $"{Size:0.00} byte";
                     case 1:
-                        return string.Format("{0:0.00} kb", Size);
+                        return $"{Size:0.00} kb";
                     case 2:
-                        return string.Format("{0:0.00} mb", Size);
+                        return $"{Size:0.00} mb";
                     case 3:
-                        return string.Format("{0:0.00} gb", Size);
+                        return $"{Size:0.00} gb";
                     case 4:
-                        return string.Format("{0:0.00} tb", Size);
+                        return $"{Size:0.00} tb";
                 }
             }
+
             return ConvertBytesToPrettyString(F, ++R);
         }
+
         private void ZombieToastLinkLabel_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("http://ZombieToast.de");
+            Process.Start("http://ZombieToast.de");
         }
 
         private void DiscordPictureBox_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("https://discord.gg/hYnRxtC");
+            Process.Start("https://discord.gg/hYnRxtC");
         }
     }
 }
